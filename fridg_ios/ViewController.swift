4de9ce22bf106aa97f2d42075a0010c46da1ce72
Dennis Gordon <dennis@idevlife.com>
2016-09-22 02:21:08 -0700

    Initial Commit

---
+++ fridg_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fridg_ios
+//
+//  Created by Dennis Gordon on 9/22/16.
+//  Copyright © 2016 Dennis Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
